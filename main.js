a3e958c8f794f1c4264b5077c31b642c23485028
AniaTofik <anna.bodal@op.pl>
2023-02-02 19:05:32 +0100

    create function addTask

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let todoInput, erroeInfo, addBtn, ulList
+let todoInput, erroeInfo, addBtn, ulList, newTodo
 
 const main = () => {
     prepareDOMElements()
@@ -8,11 +8,25 @@ const prepareDOMElements = () => {
     todoInput = document.querySelector('.todo-input')
     errorInfo = document.querySelector('.error-info')
     addBtn = document.querySelector('.btn-add')
-    ulList = document.querySelector('.todoList ul')
+    ulList = document.querySelector('.todolist ul')
     
 }
 const prepareDOMEvents = () => {
-    
+    // nadajemy nasłuchiwanie
+    addBtn.addEventListener('click', addTask)
 }
+
+const addTask = () => {
+    if (todoInput.value !== '' ){
+        newTodo = document.createElement('li')
+        newTodo.textContent = todoInput.value
+         ulList.append(newTodo)
+         todoInput.value = ''
+         errorInfo.textContent = ''
+    } else {
+        errorInfo.textContent = 'Wpisz tre zadania'
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', main)
